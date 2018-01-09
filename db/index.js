3ef5f547a119f60e1ee0ff9454d7b993dc0ff500
James Evans <james3780@gmail.com>
2018-01-09 15:35:31 -0800

    Added get and save functions, and schema

--- db/index.js
+++ db/index.js
@@ -9,8 +9,10 @@ const db = mongoose.createConnection(uri);
 
 
 let topicSchema = mongoose.Schema({
-  headline: String,
-  description: String,
+  id:            Number,
+  headline:      String,
+  description:   String,
+  timeStamp:     Date
 });
 
 
@@ -20,7 +22,45 @@ let Topic = db.model('Topic', topicSchema);
 //let List = db.model('List, listchema);
 //let User = db.model('User', userSchema);
 //let Organization = db.model('Organization', sessionSchema);
+let getTopics = (callback) => {
 
+  Topic.find({}, function(err, result) {
+    if (err) throw err;
+    callback(null, result);
+  });
+};
+
+// Save Topics to MongoDB
+let save = (topics) => {
+  
+  // for each topic object in topics array
+  topics.forEach((topic) => {
+
+    let name = topic.name;
+    name = new Topic({ 
+      id:           topic.owner.id,
+      headline:     topic.headline,
+      description:  topic.description,
+      timeStamp:    topic.timeStamp
+    });
+
+    Topic.update({ id: topic.id }, name, { upsert: true }, (err, name) => {
+      if (err) {
+        return console.error(err);
+      }
+        console.log("Success")
+    });
+  
+    // name.save(function (err, name) {
+    //   if (err) {
+    //     return console.error(err);
+    //   }
+    // });
+  });
+};
+
+module.exports.save = save;
+module.exports.getTopics = getTopics;
 module.exports.topics = Topic;
 // module.exports.users = User;
 // module.exports.comments = Comment;
