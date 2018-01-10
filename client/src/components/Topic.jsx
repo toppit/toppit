08a17768f601b363cb0a2f46c177c2ae7827364b
Jarrod Powell <jpowell1032@gmail.com>
2018-01-10 15:52:54 -0800

    upvote function updates data whether you previously clicked button or not
    still needs to display change in vote count and state to user

--- client/src/components/Topic.jsx
+++ client/src/components/Topic.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Card, Button, Icon} from 'semantic-ui-react';
+import http from 'axios';
 
 class Topic extends React.Component {
   constructor(props) {
@@ -14,16 +15,32 @@ class Topic extends React.Component {
   increaseUpvoteCount (topic) {
     //if neutral state
     var plusOrMinusCount = 0
-    if (!this.state.upVoteState) {
+    if (!this.state.upvoteState) {
       plusOrMinusCount = 1;
+      this.setState({
+        upvoteState: true
+      })
     } else {
       plusOrMinusCount = -1;
+       this.setState({
+        upvoteState: false
+      })     
      }
     // http PUT request to server to increase/decrease upvote count
     //use axios
-    this.setState({
-      upvotes: topic.upvotes++
+    http.patch('/topic', {
+      _id: topic._id,
+      upvote: plusOrMinusCount
     })
+      .then( ({data}) => {
+        console.log('where is my data', data);
+        this.setState({
+          upvotes: data.upvotes
+        })
+      })
+      .catch( (error) => {
+        console.log(error);
+      })
   }
 
   renderTopicDetailedView () {
@@ -32,6 +49,7 @@ class Topic extends React.Component {
   }  
 
   render() {
+    // if (this.s)
     return (
       <Card fluid>
         <Card.Content header={this.props.topic.headline} />
@@ -40,8 +58,9 @@ class Topic extends React.Component {
           <Button
             content='UpVote'
             icon='heart'
-            label={{ as: 'a', basic: true, content: this.props.topic.votes || 0}}
+            label={{ as: 'a', basic: true, content: this.props.topic.upvotes || 0}}
             labelPosition='right'
+            onClick={ () => this.increaseUpvoteCount(this.props.topic)}
           />
           <Icon name='comments' />
           {this.props.topic.comments || 0} comments
