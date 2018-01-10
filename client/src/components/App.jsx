f3e698971d497800cba81a9f4846bb12bfed8f7c
Jarrod Powell <jpowell1032@gmail.com>
2018-01-09 17:30:04 -0800

    code refactoring

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -9,13 +9,20 @@ class App extends React.Component {
     console.log('clicked new topic! This will show new topic view page')
   }
 
+  postNewTopic () {
+    //do server request to add new topic to database 
+    //then get new topic and render new list to topic list.
+  }
+
   render() {
     // console.log('Hello');
     return (
       <div>
       <div><h1>Toppit</h1></div>
       <nav>Login Goes Here</nav>
-      <button type="button" 
+      <button 
+      onNewTopic={this.postNewTopic.bind(this)}
+      type="button" 
       name="newTopicView" 
       onClick={() => this.renderNewTopicView()}>
       Add new Topic!
