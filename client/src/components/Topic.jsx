84d3f32beb9045360ff2dd455b35100fc826b470
Jarrod Powell <jpowell1032@gmail.com>
2018-01-09 16:17:04 -0800

    Render topic data and implement increase upvotecount function

--- client/src/components/Topic.jsx
+++ client/src/components/Topic.jsx
@@ -1,12 +1,36 @@
 import React from 'react'
 
-export const Topic = ({topic, handleClick}) => (
+export class Topic extends React.Component {
+  constructor(props) {
+    super(props);
 
-  <div onClick={handleClick()}>
-    <div>{topic.title}</div>
-    <div>{topic.upvotes}
-    <div>{topic.numberOfComments}</div>
-    <button type="button" name="upvote">Upvote</button>
-  </div>
-  )
+    this.state = {
+      upvotes: this.props.topic.upvotes
+    }
+  } 
+
+  increaseUpvoteCount (topic) {
+    this.setState({
+      upvotes: topic.upvotes++
+    })
+  }
+
+  render() {
+    return (
+      <div>
+          <div onClick={ () => this.props.handleClick()}>{this.props.topic.title}</div>
+          <div>upvotes: {this.state.upvotes} </div>
+          <div>comments: {this.props.topic.numberOfComments}</div>
+          <button 
+            type="button" 
+            name="upvote"
+            onClick={ () => this.increaseUpvoteCount(this.props.topic)}
+            >Upvote
+          </button>
+      </div>
+      )
+  }  
+}
+
+
 
