f100e2c6addb4f115e52a6d3296aac9a45587ad6
Jarrod Powell <jpowell1032@gmail.com>
2018-01-09 16:16:37 -0800

    App renders Topic List and nav bar

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React from 'react'
 import NewTopic from './NewTopic.jsx'
+
+
 class App extends React.Component {
   constructor() {
     super();
   }
 
   render() {
+
+
     return (
-      <div><h1>Hello World</h1>
+      <div>
+      <div><h1>Toppit</h1></div>
+      <nav>Login Goes Here</nav>
+      <TopicList />
       <NewTopic />
       </div>
     );
