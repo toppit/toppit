4c63e074bac2a76fdab8d8cdd0b3a88a87510e70
Mike Sutherland <mjfsutherland@gmail.com>
2018-01-09 20:47:44 -0800

    updating endpoints and db for posting and getting topics

--- server/index.js
+++ server/index.js
@@ -16,14 +16,27 @@ app.use(express.static(path.join(__dirname, '../client/dist')));
 app.get('/topics', (req, res) => {
   db.getTopics((error, result) => {
     if (error) {
-      res.statusCode(503).end();
+      res.status(503).end();
       console.log(error.message);
       return;
     }
-    res.statusCode(200).send(result);
+    res.status(200).send(result);
   });
 });
 
+// GET all topics from the server
+app.post('/topic', (req, res) => {
+  console.log(req.body);
+
+  db.saveTopic(req.body, (error, result) => {
+    if (error) {
+      res.status(503).end();
+      console.log(error.message);
+      return;
+    }
+    res.status(200).send(result);
+  });
+});
 
 
 app.listen(port, () => console.log(`listening on port ${port}!`));
