b50617a96ef59f63083334ea8cfb289deda0505b
Jarrod Powell <jpowell1032@gmail.com>
2018-01-09 16:27:15 -0800

    create example data of topics
    an array of 2 object, each with title, upvote count, and comment count

---
+++ client/src/exampleData.js
@@ -0,0 +1,9 @@
+export const exampleData = [{
+        title: 'Coffee Maching in bathrooms',
+        upvotes: 20,
+        numberOfComments: 10
+      }, {
+        title: 'Greenfield planning not enough time',
+        upvotes: 5,
+        numberOfComments: 23
+      }]
