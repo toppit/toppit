1cf5f16a6db3205e2b02d1f5d23eee2ae31cf75d
Jarrod Powell <jpowell1032@gmail.com>
2018-01-09 16:24:00 -0800

    Include Add topic button
    this has no functionality yet, but will eventually render new topic view page

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -5,12 +5,21 @@ class App extends React.Component {
     super();
   }
 
+  renderNewTopicView() {
+    console.log('clicked new topic! This will show new topic view page')
+  }
+
   render() {
     // console.log('Hello');
     return (
       <div>
       <div><h1>Toppit</h1></div>
       <nav>Login Goes Here</nav>
+      <button type="button" 
+      name="newTopicView" 
+      onClick={() => this.renderNewTopicView()}>
+      Add new Topic!
+      </button>
       <TopicList />
       </div>
     );
