601ff9ead90ef71030b8e69006acbb3b99fd7d5d
Jarrod Powell <jpowell1032@gmail.com>
2018-01-12 10:04:16 -0800

    Add new comment example data

--- client/src/exampleData.js
+++ client/src/exampleData.js
@@ -1,9 +1,21 @@
 export const exampleData = [{
-        title: 'Coffee Maching in bathrooms',
+        headline: 'Coffee Machine in bathrooms',
+        description: 'alot more words describing this',
         upvotes: 20,
-        numberOfComments: 10
+        numberOfComments: 10,
+        comments: exampleCommentData
       }, {
         title: 'Greenfield planning not enough time',
         upvotes: 5,
         numberOfComments: 23
       }]
+
+const exampleCommentData = [{
+  username: 'jrodchu',
+  description: 'This is sooo funny! ...chu',
+  timestamp: 'yesterday'
+}, {
+  username: 'sfkiwi',
+  description: 'A really good point! I agree',
+  timeStamp: 'a few seconds ago'
+}]      
