d516e3e107d57c28fc61c13653423d89f0544f37
James Evans <james3780@gmail.com>
2018-01-12 09:48:43 -0800

    Add emotion property to topicSchema

--- db/schemas.js
+++ db/schemas.js
@@ -2,23 +2,24 @@ const topicSchema = db.Schema({
     headline:      String,
     description:   String,
     timeStamp:     Date,
-    upvotes:       Number
+    upvotes:       Number,
+    emotion:       String
   });
 
   const commentSchema = db.Schema({
     text:       String,
     timeStamp:  Date,
-    author_id:  Number,
+    authorId:   Number,
     upvotes:    Number
   });
 
   const userSchema = db.Schema({
-    user_name:    String,
-    full_name:    String,
+    userName:    String,
+    fullName:    String,
     password:     String,
-    topic_id:     Number,
-    list_id:      Number,
-    comment_id:   Number
+    topicId:     Number,
+    listId:      Number,
+    commentId:   Number
   });
 
   const schema = {
