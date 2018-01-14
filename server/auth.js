ed10edbeb91a7bc0b8f120a59c045f5483e3e0ab
Mike Sutherland <mjfsutherland@gmail.com>
2018-01-14 11:02:36 -0800

    logging process env

--- server/auth.js
+++ server/auth.js
@@ -11,6 +11,7 @@ const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 const GitHubStrategy = require('passport-github').Strategy;
 const db = require('../db');
 
+console.log(process.env);
 if (process.env.HEROKU_APP_NAME) {
   console.log('Heroku App Name: ', process.env.HEROKU_APP_NAME);
   process.env.SERVER_IP = `https://${process.env.HEROKU_APP_NAME}`;
